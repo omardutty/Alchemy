0eefc14a0154399a21ec1ce1b01d42b08aee85c5
Oscar Espejo Sagués <xxgreysoulxx@gmail.com>
2017-04-30 22:34:36 +0200

    Funcion de lectura DEFINITIVA, y finalmente la combinacion de elementos, aunque da algun que otro error

--- Alchemy/main.cpp
+++ Alchemy/main.cpp
@@ -11,51 +11,39 @@
 //Vector de strings global
 std::vector<std::string> elemento({"Air", "Earth", "Fire", "Water"});
 //Mapa de los elementos, lo he vuelto a modificar para que entren pairs
-//std::unordered_map<std::pair<std::string, std::string>,std::string> mapa;
+std::unordered_map<std::string, std::string> mapa;
 //Puntuacion del player
 int puntuacionplayer = 0;
 
-/*
+
 void lecturadelfichero() {
-	std::ifstream archivo("elements.dat");
+	std::ifstream archivo;						           //inicializamos una variable de tipo ifstring
+	std::string texto;
+	archivo.open("elements.dat", std::ios::in);			   //abrimos el archivo de los elementos
 
-	if (archivo.is_open())
-	{
-		std::string line;
-		std::string elemento1; //aqui guardaremos el primer elemento
-		std::string elemento2; //aqui guardaremos el segundo elemento
-		std::string combinacion; //aqui la combinacion de ambos
-
-		//http://www.cplusplus.com/reference/algorithm/find/
-		int igual = line.find('='); //tiene que ser int ja que find devuelve una posicion
-		int suma = line.find('+');
-
-		//Uso el getline para cojer desde el archivo cada linia
-		while (getline(archivo, line)) {
-			//Separacion va desde el caracter 0 al =
-			combinacion = line.substr(0, igual);
-			//Primer elemento desde el igual hasta la suma
-			elemento1 = line.substr(igual, suma);
-			//Segundo elemento desde la suma hasta el tamaño de la linia
-			elemento2 = line.substr(suma, line.size());
-			//Insertamos en el mapa la lectura del fichero
-			mapa.insert({ { elemento1 , elemento2 }, combinacion });
-
-		}
-	}
-	else
-	{
-		std::cout << "La carga del archivo ha fracasado" << std::endl;
+	if (archivo.fail()) {                                  //en caso de que no se pueda abrir el archivo paramos el sistema
+		std::cout << "The file is not here! Sorry buddy but I can't open it!";
 		system("pause");
 	}
 
-	archivo.close();
+	while (!archivo.eof()) {							   //mientras no llegue al final del archivo
+		std::getline(archivo, texto);                      //cojemos toda la linia i la guardamos como un string --> http://www.cplusplus.com/reference/string/string/getline/
+		int j, i = 0;										   
+		while (i < texto.length() && j == 0) {             //mientras i sea mas pequeño que el tamaño del texto y j sea 0 
+			if (texto[i] == '=') j = i;                    //separamos la key que seria "Lava" de la suma de los elementos "Earth + Fire"
+			++i;
+		}
+		std::string key = texto.substr(0, j - 1);          //cojemos desde el principio de la linia, 0 , hasta la poscion j-1 que evivale al =
+		std::string cont = texto.substr(j + 2, texto.length() - (j + 2)); //a j le sumamos 2 por los espacios, y posteriormente al legth le restamos j + 2 para poder cojer el otro string "Fire + Earth"  
+		mapa[cont] = key;                                  //el contenido del mapa es igual a la key "Fire + Earth = Lava"
+	}
+	archivo.close(); 
 
-	//For que pemite imprimir el documento de elementos
+	/*//For que pemite imprimir el documento de elementos
 	for (auto it = mapa.begin(); it != mapa.end(); ++it){
 	std::cout << " " << it->first << " : " << it->second << std::endl;
-	}
-}*/
+	}*/
+}
 
 //Le da los elementos basicos al player
 void AddBasics() {
@@ -70,7 +58,7 @@ void Add() {
 	int posicion;
 	std::cin >> posicion;
 	std::string adder;
-	adder = elemento[posicion];//guardamos el elemento que queremos y posteriormente hacemos un push_back de este
+	adder = elemento[posicion];							//guardamos el elemento que queremos y posteriormente hacemos un push_back de este
 	elemento.push_back(adder);
 }
 
@@ -86,14 +74,13 @@ void Info() {
 	int posicion;
 	std::cin >> posicion;
 	std::string url;
-	url = "https://en.wikipedia.org/wiki/" + elemento[posicion];
+	url = "https://en.wikipedia.org/wiki/" + elemento[posicion];   //Union de los dos "strings", para poder buscar en wikipedia
 	ShellExecuteA(nullptr, "open", url.c_str(), nullptr, nullptr, SW_SHOWNORMAL);
 }
 
 //Funcion que te sortea los elementos
 void Sort() {
-	//Funcion tal cual ya esta bien
-	std::sort(elemento.begin(), elemento.end());
+  std::sort(elemento.begin(), elemento.end());
 }
 
 //Funcion del clean problema solo borra cuando estan ordenados alfabeticamente
@@ -101,7 +88,6 @@ void Clean() {
 	std::sort(elemento.begin(), elemento.end());
 	elemento.erase(std::unique(elemento.begin(), elemento.end()), elemento.end());
 }
-
 
 //Funcion de Help para el player
 void Help() {
@@ -117,6 +103,33 @@ void Help() {
 	std::cout << std::endl;
 	std::cout << "If you type clean you will delete all repeteaded elements" << std::endl;
 	std::cout << std::endl;
+}
+
+void Combinacion() {
+	int num1, num2;
+	std::cin >> num1 >> num2;												    //pedimos los dos numeros al player
+	std::string elemento1, elemento2;											//generamos los dos strings para almacenarlos
+	elemento1 = elemento[num1];													//asignamos el valor de la posicion del vector cogiendo el elemento que necesitamos
+	elemento2 = elemento[num2];
+
+	std::string aux = " + ";
+	std::string comb = elemento1 + aux + elemento2;								//la suma de los tres strings es la combinacion, ejemplo  "Fire + Water"
+	std::unordered_map<std::string, std::string>::iterator iterador;			//creacion del iterador que ira recorriendo el map
+
+	iterador = mapa.find(comb);													//buscamos la combinacion, si existe
+	if (iterador != mapa.end()) {												//si el iterador esta antes del fin del archivo existe si no, no
+
+		elemento.erase(elemento.begin() + num1);								//borramos los elementos combinados
+		elemento.erase(elemento.begin() + num2); 
+
+		std::string result = iterador->second;								    //second hace referencia a lo que hay al otro lado del igual en el archivo de lectura
+		std::cout << "Your new elements is : " << result << std::endl;
+		elemento.push_back(result);												//pusheamos el nuevo elemento
+		puntuacionplayer++;														//aumentamos la puntuacion del player
+	}
+	else {
+		std::cout << "The combination have failed! I'm sorry!" << std::endl;
+	}
 }
 
 void gamemenu() {
@@ -137,10 +150,20 @@ void gamemenu() {
 	std::cout << " " << std::endl;
 }
 
-//Funcion que controla el curso del juego, las funciones
-void ingame(std::string player) {
-	//Funcion endgame provisional hasta que controlemos la puntuacion del player
-	while (player != "endgame")
+
+
+void main() {
+	lecturadelfichero();
+	gamemenu();
+	// Printear los basics al inicio del juego
+	std::cout << "You have those elements :" << std::endl;
+		for (int i = 0; i < elemento.size(); i++) {
+		std::cout << "     " << i << "  " << elemento[i] << std::endl;
+	    }
+	std::string player;
+	std::cin >> player;
+
+	while (puntuacionplayer != 100)
 	{
 		if (player == "addbasics") {
 			AddBasics();
@@ -163,37 +186,19 @@ void ingame(std::string player) {
 		else if (player == "info") {
 			Info();
 		}
+		else if (player == "comb") {
+			Combinacion();
+		}
 
 		else {
 			std::cout << "I don't understand that buddy! Type help for help!" << std::endl;
 		}
-
-		std::cout << "You have those elements :" << std::endl;
-		for (int i = 0; i <= elemento.size() - 1; i++) {
-			std::cout << "     " << i << "  " << elemento[i] << std::endl;
-		}
+				std::cout << "Your current score : " << puntuacionplayer << std::endl;
+				std::cout << "You have those elements :" << std::endl;
+				for (int i = 0; i <= elemento.size() - 1; i++) {
+					std::cout << "     " << i << "  " << elemento[i] << std::endl;
+					}
 		std::cin >> player;
 		system("cls");
 	}
-}
-
-
-
-void main() {
-	//lecturadelfichero();
-	gamemenu();
-
-	// Printear los basics al inicio del juego
-	std::cout << "Your current score :" << puntuacionplayer << std::endl;
-	std::cout << "You have those elements :" << std::endl;
-
-		for (int i = 0; i < elemento.size(); i++) {
-		std::cout << "     " << i << "  " << elemento[i] << std::endl;
-		}
-
-	std::string player;
-	std::cin >> player;
-	std::cin.clear();
-	std::cin.ignore(std::cin.rdbuf()->in_avail());
-	ingame(player); //llamamos a la funcion que controla el curso del juego 
 }
